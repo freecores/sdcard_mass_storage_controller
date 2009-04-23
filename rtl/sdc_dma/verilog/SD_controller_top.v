88d8753e281315173265172a38eac2fb891cf7dc
tac2 <tac2@b3eed8dd-2e6a-4d6b-8242-701a975db75b>
2009-04-23 09:02:34 +0000

    Comments added

--- rtl/sdc_dma/verilog/SD_controller_top.v
+++ rtl/sdc_dma/verilog/SD_controller_top.v
@@ -52,36 +52,32 @@ output  	[2:0] m_wb_cti_o;
 output	[1:0]	 m_wb_bte_o;
 //SD port
 
-input wire [3:0] sd_dat_dat_i;
-output wire [3:0] sd_dat_out_o;
-output wire sd_dat_oe_o;
+input wire [3:0] sd_dat_dat_i;   //Data in from SDcard
+output wire [3:0] sd_dat_out_o; //Data out to SDcard
+output wire sd_dat_oe_o; //SD Card tristate Data Output enable (Connects on the SoC TopLevel)
 
-input wire sd_cmd_dat_i;
-output wire sd_cmd_out_o;
-output wire sd_cmd_oe_o;
+input wire sd_cmd_dat_i; //Command in from SDcard
+output wire sd_cmd_out_o; //Command out to SDcard
+output wire sd_cmd_oe_o; //SD Card tristate CMD Output enable (Connects on the SoC TopLevel)
 
-output wire sd_clk_o_pad;
 //IRQ
 `ifdef IRQ_ENABLE
    output int_a, int_b, int_c ; 
   `endif
+  
 reg wb_ack_o;
 reg wb_inta_o;
 reg new_cmd;
 
 
-
-
-
 `define SUPPLY_VOLTAGE_3_3
 `define SD_CARD_2_0
-`define SD_BUS_WIDTH_
-//Register addres
+
+//Register Addreses 
 `define argument 8'h00
 `define command 8'h04
 `define status 8'h08
 `define resp1 8'h0c
-
 `define controller 8'h1c
 `define block 8'h20
 `define power 8'h24
@@ -91,16 +87,11 @@ reg new_cmd;
 `define error_isr 8'h34  
 `define normal_iser 8'h38
 `define error_iser 8'h3c
-//`define normal_isiger 8'h40
-//`define error_isiger 8'h44
 `define capa 8'h48
 `define clock_d 8'h4c
 `define bd_status 8'h50
 `define bd_isr 8'h54 
 `define bd_iser 8'h58 
-
-
-//bd_isiger 8'h5c
 `define bd_rx 8'h60  
 `define bd_tx 8'h80  
 
@@ -116,7 +107,6 @@ assign m_wb_sel_o = 4'b1111;
 
 parameter block_size_reg = `BLOCK_SIZE ; //512-Bytes
 
-
 `ifdef SD_BUS_WIDTH_4
      parameter controll_setting_reg =16'b0000_0000_0000_0010;
 `else  
@@ -124,55 +114,50 @@ parameter block_size_reg = `BLOCK_SIZE ; //512-Bytes
 `endif
      parameter capabilies_reg =16'b0000_0000_0000_0000;
    
-//Registers  
+//Buss accessible registers    
 reg [31:0] argument_reg;
 reg [15:0] cmd_setting_reg;
 reg [15:0] status_reg;
 reg [31:0] cmd_resp_1;
-
 reg [7:0] software_reset_reg; 
 reg [15:0] time_out_reg;   
 reg [15:0]normal_int_status_reg; 
 reg [15:0]error_int_status_reg;
 reg [15:0]normal_int_signal_enable_reg;
 reg [15:0]error_int_signal_enable_reg;
-
-
-reg [7:0]clock_divider;
+reg [7:0] clock_divider;
 reg [15:0] Bd_Status_reg;   
 reg [7:0] Bd_isr_reg;
 reg [7:0] Bd_isr_enable_reg;
 
-
+//Register Controll
 reg Bd_isr_reset;
 reg normal_isr_reset;
 reg error_isr_reset;
 
-//Add blockram for bigger BD defines.   
-   
-   
+//Wires from SD_CMD_MASTER Module 
 wire [15:0] status_reg_w;
 wire [31:0] cmd_resp_1_w;
-
 wire [15:0]normal_int_status_reg_w;
 wire [15:0]error_int_status_reg_w; 
  
-wire sd_clk_i;
-wire bd_rx_stb;
-reg rx_in; 
+
+
+//Internal Delayed Ack
 reg int_ack;
  
-reg we_m_rx_bd;
-reg re_m_rx_bd;
-reg [`RAM_MEM_WIDTH-1:0] dat_in_m_rx_bd;
-wire [`RAM_MEM_WIDTH-1:0] dat_out_m_rx_bd;
-wire [`BD_WIDTH-1 :0] free_bd_rx_bd;
-wire new_rx_bd;
-reg re_s_rx_bd;
+//Rx Buffer  Descriptor internal signals
+reg we_m_rx_bd;  //Write enable Master side Rx_bd
+reg re_m_rx_bd;  //Read enable Master side Rx_bd
+reg [`RAM_MEM_WIDTH-1:0] dat_in_m_rx_bd; //Data in to Rx_bd from Master
+wire [`RAM_MEM_WIDTH-1:0] dat_out_m_rx_bd; //Data out from Rx_bd to Master
+wire [`BD_WIDTH-1 :0] free_bd_rx_bd; //NO free Rx_bd
+wire new_rx_bd;  // New Bd writen
+reg re_s_rx_bd; // Read enable Slave side Rx_bd
+reg a_cmp_rx_bd; //A Completed transmison of a Bd
+wire [`RAM_MEM_WIDTH-1:0] dat_out_s_rx_bd; //Data out from Rx_bd to Slave
 
-reg a_cmp_rx_bd;
-wire [`RAM_MEM_WIDTH-1:0] dat_out_s_rx_bd;
-
+//Tx Buffer Descriptor internal signals
 reg we_m_tx_bd;
 reg re_m_tx_bd;
 reg [`RAM_MEM_WIDTH-1:0] dat_in_m_tx_bd;
@@ -181,30 +166,46 @@ wire [`BD_WIDTH-1 :0] free_bd_tx_bd;
 wire new_tx_bd;
 reg re_s_tx_bd;
 reg a_cmp_tx_bd;
-
 wire [`RAM_MEM_WIDTH-1:0] dat_out_s_tx_bd;
 reg [1:0] we;
- 
+wire [7:0] bd_int_st_w; //Wire to BD status register
+
+//Wires for connecting Bd registers with the SD_Data_master module
 wire re_s_tx_bd_w;
 wire a_cmp_tx_bd_w;
 wire re_s_rx_bd_w;
 wire a_cmp_rx_bd_w;
-wire we_req_t;
-wire [31:0] cmd_arg_m;
-wire [31:0] cmd_set_m;
-wire [31:0] sys_adr;
-wire cmd_busy;
-wire [3:0]data_out;
-reg we_ack;
+
+wire write_req_s; //SD_Data_master want acces to the CMD line.
+wire cmd_busy; //CMD line busy no access granted
+reg we_ack; //CMD acces granted
+wire [31:0] cmd_arg_s; //SD_Data_master CMD Argument
+wire [31:0] cmd_set_s; //SD_Data_master Settings Argument
+wire [31:0] sys_adr; //System addres the DMA whil Read/Write to/from
+wire [1:0]start_dat_t; //Start data transfer
+
+//Signals to Syncronize busy signaling betwen Wishbone access and SD_Data_master access to the CMD line (Also manage the status reg uppdate)
 reg int_busy;
 assign cmd_busy = int_busy | status_reg[0];
-wire sd_clk_o;
+wire status_reg_busy;
+reg cmd_int_busy;
 
-wire [7:0] bd_int_st_w;
+//Wires from SD_DATA_SERIAL_HOST_1 to the FIFO
+wire [`SD_BUS_W -1 : 0 ]data_in_rx_fifo;
+wire [31: 0 ] data_fout_tx_fifo;
+wire [31:0] m_wb_dat_o_rx;
+wire [3:0] m_wb_sel_o_tx;
+wire [31:0] m_wb_adr_o_tx;
+wire [31:0] m_wb_adr_o_rx;
+
+//SD clock 
+wire sd_clk_i; //Sd_clk provided to the system
+wire sd_clk_o; //Sd_clk used in the system 
+output sd_clk_o_pad;
+//sd_clk_o to be used i set here
 `ifdef SD_CLK_BUS_CLK
   assign sd_clk_i = wb_clk_i;
 `endif 
-
 
 `ifdef SD_CLK_STATIC
    assign sd_clk_o = sd_clk_i;
@@ -218,8 +219,8 @@ wire [7:0] bd_int_st_w;
  .SD_CLK  (sd_clk_o)  
 );
 `endif
- assign sd_clk_o_pad  = sd_clk_o ;
-wire [1:0]st_dat_t;
+assign sd_clk_o_pad  = sd_clk_o ;
+
 
 SD_CMD_MASTER cmd_master_1
 (
@@ -237,15 +238,13 @@ SD_CMD_MASTER cmd_master_1
 .STATUS_REG (status_reg_w),
 .TIMEOUT_REG (time_out_reg),
 .RESP_1_REG (cmd_resp_1_w),
-
 .ERR_INT_REG (error_int_status_reg_w),
 .NORMAL_INT_REG (normal_int_status_reg_w),
 .ERR_INT_RST (error_isr_reset),
 .NORMAL_INT_RST (normal_isr_reset),
 .CLK_DIVIDER (clock_divider),
-.st_dat_t (st_dat_t)
+.st_dat_t (start_dat_t)
 );
-
 
 
 SD_DATA_MASTER data_master_1 
@@ -255,7 +254,7 @@ SD_DATA_MASTER data_master_1
 .new_tx_bd  (new_tx_bd),
 .dat_in_tx (dat_out_s_tx_bd),
 .free_tx_bd  (free_bd_tx_bd),
-.ack_i_s_tx (  ack_o_s_tx         ),
+.ack_i_s_tx (  ack_o_s_tx ),
 .re_s_tx (re_s_tx_bd_w), 
 .a_cmp_tx (a_cmp_tx_bd_w),
 .new_rx_bd  (new_rx_bd),
@@ -265,12 +264,12 @@ SD_DATA_MASTER data_master_1
 .re_s_rx (re_s_rx_bd_w), 
 .a_cmp_rx (a_cmp_rx_bd_w),
 .cmd_busy (cmd_busy),
-.we_req (we_req_t),
+.we_req (write_req_s),
 .we_ack (we_ack),
 .d_write ( d_write   ),
 .d_read (  d_read  ),
-.cmd_arg ( cmd_arg_m),
-.cmd_set ( cmd_set_m),
+.cmd_arg ( cmd_arg_s),
+.cmd_set ( cmd_set_s),
 .cmd_tsf_err (normal_int_status_reg[15]) ,
 .card_status (cmd_resp_1[12:8])   ,
 .start_tx_fifo (start_w),
@@ -288,14 +287,7 @@ SD_DATA_MASTER data_master_1
 .CIDAT  (cidat_w)
 );
  
- 
- 
- //Placeholder to rx fifo
- wire [`SD_BUS_W -1 : 0 ]data_in_rx_fifo;
 
- wire stop_transf;
- wire [31: 0 ] data_out_tx_fifo;
- 
 SD_DATA_SERIAL_HOST SD_DATA_SERIAL_HOST_1(
 .sd_clk     (sd_clk_o),
 .rst        (wb_rst_i | software_reset_reg[0]),
@@ -306,14 +298,12 @@ SD_DATA_SERIAL_HOST SD_DATA_SERIAL_HOST_1(
 .DAT_oe_o   (sd_dat_oe_o),
 .DAT_dat_o  (sd_dat_out_o),
 .DAT_dat_i  (sd_dat_dat_i),
-.start_dat  (st_dat_t),
+.start_dat  (start_dat_t),
 .ack_transfer       (ack_transfer),
 .busy_n       (busy_n),
 .transm_complete     (trans_complete ),
 .crc_ok         (crc_ok)
 );
-
-
 
 
 SD_Bd rx_bd
@@ -349,17 +339,7 @@ SD_Bd tx_bd
 .dat_out_s (dat_out_s_tx_bd)
 
 );
-//SD_Bd tx_bd
-//(
 
-
-
-    
-
-wire [31:0] m_wb_dat_o_rx;
-wire [3:0] m_wb_sel_o_tx;
-wire [31:0] m_wb_adr_o_tx;
-wire [31:0] m_wb_adr_o_rx;
 
 SD_FIFO_TX_FILLER FIFO_filer_tx (
 .clk (wb_clk_i),
@@ -399,6 +379,7 @@ SD_FIFO_RX_FILLER FIFO_filer_rx (
 .full (full_rx)
 );
 
+//MUX For WB master acces granted to RX or TX FIFO filler
 assign m_wb_cyc_o = start_w ? m_wb_cyc_o_tx :start_r ?m_wb_cyc_o_rx: 0;
 assign m_wb_stb_o = start_w ? m_wb_stb_o_tx :start_r ?m_wb_stb_o_rx: 0;
 assign m_wb_dat_o = m_wb_dat_o_rx;
@@ -411,66 +392,51 @@ assign int_b = error_int_status_reg & error_int_signal_enable_reg;
 assign int_c =  Bd_isr_reg & Bd_isr_enable_reg;
 `endif
 
-always @ (re_s_tx_bd_w or a_cmp_tx_bd_w or  re_s_rx_bd_w or a_cmp_rx_bd_w or we_req_t) begin
+always @ (re_s_tx_bd_w or a_cmp_tx_bd_w or  re_s_rx_bd_w or a_cmp_rx_bd_w or write_req_s) begin
   re_s_tx_bd<=re_s_tx_bd_w;
   a_cmp_tx_bd <=a_cmp_tx_bd_w;
   re_s_rx_bd <=re_s_rx_bd_w; 
   a_cmp_rx_bd<=a_cmp_rx_bd_w;
 end
 
+//Set Bd_Status_reg
 always @ ( free_bd_tx_bd or free_bd_rx_bd ) begin
   Bd_Status_reg[15:8]=free_bd_rx_bd;
   Bd_Status_reg[7:0]=free_bd_tx_bd;
 end
 
-wire status_reg_busy;
-reg cmd_int_busy;
-
 always @( cmd_resp_1_w  or error_int_status_reg_w or normal_int_status_reg_w ) begin
-
 cmd_resp_1<= cmd_resp_1_w;
 normal_int_status_reg<= normal_int_status_reg_w  ;
 error_int_status_reg<= error_int_status_reg_w  ;
 end  
 
-
-
-always @ (   cidat_w or cmd_int_busy or status_reg_w or status_reg_busy or bd_int_st_w) begin
+always @ ( cidat_w or cmd_int_busy or status_reg_w or status_reg_busy or bd_int_st_w) begin
  status_reg[0]<= status_reg_busy;
  status_reg[15:1]<=  status_reg_w[15:1]; 
-
  status_reg[1] <= cidat_w; 
  Bd_isr_reg<=bd_int_st_w;
- end
- assign status_reg_busy = cmd_int_busy ? 1'b1: status_reg_w[0];
+end
  
-  
-	// generate acknowledge output signal
-//always @(posedge wb_clk_i) begin
-//	  wb_ack_o <=  wb_cyc_i & wb_stb_i & ~wb_ack_o & int_ack; 
-//end
-   
+//cmd_int_busy is set when an internal access to the CMD buss is granted then immidetly uppdate the status busy bit to prevent buss access to cmd
+assign status_reg_busy = cmd_int_busy ? 1'b1: status_reg_w[0];
  
-  
+//WB write
 always @(posedge wb_clk_i or posedge wb_rst_i)
-	
 	begin
 	  we_m_rx_bd <= 0;
    	we_m_tx_bd <= 0;
 	  new_cmd<= 1'b0 ;
-	 we_ack <= 0;
-	 int_ack =  1;
-	 cmd_int_busy<=0;
+	  we_ack <= 0;
+	  int_ack =  1;
+	  cmd_int_busy<=0;
      if ( wb_rst_i )begin
 	    argument_reg <=0;
       cmd_setting_reg <= 0;
 	    software_reset_reg <= 0;
 	    time_out_reg <= 0;
 	    normal_int_signal_enable_reg <= 0;
-	    error_int_signal_enable_reg <= 0;
-	    //normal_int_signal_enable_reg <= 0;
-	    //error_int_signal_enable_reg <= 0;
-	   
+	    error_int_signal_enable_reg <= 0;	  
 	    clock_divider <=`RESET_CLK_DIV;
 	    int_ack=1 ;
 	    we<=0;
@@ -485,8 +451,8 @@ always @(posedge wb_clk_i or posedge wb_rst_i)
 	    normal_isr_reset<=0;
 	    error_isr_reset<=0;
 	  end
-	  else if ((wb_stb_i  & wb_cyc_i) || wb_ack_o )begin //CS
-	    Bd_isr_reset<=0;
+	  else if ((wb_stb_i  & wb_cyc_i) || wb_ack_o )begin 
+	    Bd_isr_reset<=0; 
 	     normal_isr_reset<=  0;
 	    error_isr_reset<=  0;
 	    if (wb_we_i) begin
@@ -503,14 +469,43 @@ always @(posedge wb_clk_i or posedge wb_rst_i)
           `timeout : time_out_reg  <=  wb_dat_i;
           `normal_iser : normal_int_signal_enable_reg <=  wb_dat_i;
           `error_iser : error_int_signal_enable_reg  <=  wb_dat_i;
-        `normal_isr : normal_isr_reset<=  1;
+          `normal_isr : normal_isr_reset<=  1;
           `error_isr:  error_isr_reset<=  1;
 	        `clock_d: clock_divider  <=  wb_dat_i;
 	        `bd_isr: Bd_isr_reset<=  1;	    
 	        `bd_iser : Bd_isr_enable_reg <= wb_dat_i ;     
 	        `ifdef RAM_MEM_WIDTH_32
+	          `bd_rx: begin
+	             we <= we+1;	           
+	             we_m_rx_bd <= 1;
+	             int_ack =  0;	
+	           if  (we[1:0]==2'b00)
+	             we_m_rx_bd <= 0;
+	           else if  (we[1:0]==2'b01) 
+	            dat_in_m_rx_bd <=  wb_dat_i;	                   
+	           else begin
+	              int_ack =  1; 
+	              we<= 0;
+	              we_m_rx_bd <= 0;
+	            end
+	           
+	        end
+	        `bd_tx: begin
+	           we <= we+1;	           
+	           we_m_tx_bd <= 1;
+	           int_ack =  0;	
+	           if  (we[1:0]==2'b00)
+	             we_m_tx_bd <= 0;
+	           else if  (we[1:0]==2'b01) 
+	            dat_in_m_tx_bd <=  wb_dat_i;                   
+	           else begin
+	             int_ack =  1; 
+	              we<= 0;
+	              we_m_tx_bd <= 0;
+	            end
+	        end
 	        
-	         `endif
+	        `endif
 	        `ifdef RAM_MEM_WIDTH_16
 	        `bd_rx: begin
 	             we <= we+1;	           
@@ -551,10 +546,10 @@ always @(posedge wb_clk_i or posedge wb_rst_i)
 	    end     	     
 	wb_ack_o =   wb_cyc_i & wb_stb_i & ~wb_ack_o & int_ack; 
 	 end
-	    else if (we_req_t) begin
+	    else if (write_req_s) begin
 	       new_cmd <=  1'b1 ; 
-	       cmd_setting_reg <=   cmd_set_m; 
-	       argument_reg  <=  cmd_arg_m ; 
+	       cmd_setting_reg <=   cmd_set_s; 
+	       argument_reg  <=  cmd_arg_s ; 
 	       cmd_int_busy<=  1; 
 	       we_ack <= 1;
 	    end  
